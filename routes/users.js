18e5855bcda4d28fa3f10e53a458fdae63f54a39
Frankroz <franjrm201518@hotmail.com>
2025-11-06 16:54:29 +0800

    first commit

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+const userController = require("../controllers/users")
+
+router.get("/:id", userController.getSingle)
+
+router.get("/", userController.getAllUsers)
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const mongodb = require("./data/database");
+const app = express();
+const env = require("dotenv").config();
+
+app.use(cors());
+
+app.use("/", require("./routes/index.js"));
+
+/* ***********************
+ * Local Server Information
+ *************************/
+const port = process.env.PORT;
+const host = process.env.HOST;
+
+/* ***********************
+ * Log statement to confirm server operation
+ *************************/
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port, () => {
+      console.log(`Database listening - app listening on ${host}:${port}`);
+    });
+  }
+});
